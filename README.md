95a467054509a7b8aea50d5724aaadacd854323a
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-13 17:43:04 +0100

    Event Object in React

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-just put kod from history in searching on some browser ⊗jsrtPmFcHP
-or [click](https://code.mu/ru/javascript/framework/react/book/prime/functions/handlers-params/)
+just put kod from history in searching on some browser ⊗jsrtPmFcEO
+or [click](https://code.mu/ru/javascript/framework/react/book/prime/functions/event-object/)
--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-// Даны три кнопки:
-
-// function App() {
-// 	return <div>
-// 		<button>act1</button>
-// 		<button>act2</button>
-// 		<button>act3</button>
-// 	</div>;
-// }
-// Сделайте так, чтобы по клику на первую кнопку через alert выводилось число 1, по клику на вторую кнопку - число 2, а по клику на третью - число 3.
-
+// Дана кнопка. По клику на нее получите объект Event и выведите его в консоль.
+// Дана кнопка. По клику на нее получите выведите в консоль event.target клика.
 function App() {
-  function showNumber(num) {
-    return alert(num);
+  function funcEv(ev) {
+    console.log(ev);
+  }
+  function funcTarget(ev) {
+    console.log(ev.target);
   }
   return (
-    <div>
-      <button onClick={()=>showNumber(1)}>act1</button>
-      <button onClick={()=>showNumber(2)}>act2</button>
-      <button onClick={()=>showNumber(3)}>act3</button>
-    </div>
+    <>
+      <button onClick={(ev) => funcEv(ev)}>click ev</button>
+      <button onClick={(ev) => funcTarget(ev)}>click target</button>
+    </>
   );
 }
 
