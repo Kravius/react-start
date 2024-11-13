40fdba1b498909e199845c7fd4b421201ad5fd0f
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-13 17:37:34 +0100

    Parameters in a JSX function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-// Дан следующий код:
+// Даны три кнопки:
 
 // function App() {
-// 	function show1() {
-// 		alert(1);
-// 	}
-
-// 	function show2() {
-// 		alert(2);
-// 	}
-
 // 	return <div>
 // 		<button>act1</button>
 // 		<button>act2</button>
+// 		<button>act3</button>
 // 	</div>;
 // }
-// Сделайте так, чтобы по клику на первую кнопку срабатывала первая функция, а по клику на вторую кнопку - вторая функция.
+// Сделайте так, чтобы по клику на первую кнопку через alert выводилось число 1, по клику на вторую кнопку - число 2, а по клику на третью - число 3.
 
 function App() {
-  function show1() {
-    alert(1);
+  function showNumber(num) {
+    return alert(num);
   }
-
-  function show2() {
-    alert(2);
-  }
-
   return (
     <div>
-      <button onClick={show1}>act1</button>
-      <button onClick={show2}>act2</button>
+      <button onClick={()=>showNumber(1)}>act1</button>
+      <button onClick={()=>showNumber(2)}>act2</button>
+      <button onClick={()=>showNumber(3)}>act3</button>
     </div>
   );
 }
+
 export default App;
