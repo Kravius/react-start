091bfd294faaabf20a831df210da677a8532e1ce
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-13 16:48:52 +0100

    Вставка массивов в JSX

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-just put kod from history in searching on some browser ⊗jsrtPmJxRS
-or  [click](https://code.mu/ru/javascript/framework/react/book/prime/jsx/returning/several/)
+just put kod from history in searching on some browser ⊗jsrtPmJxVAr
+or  [click](https://code.mu/ru/javascript/framework/react/book/prime/jsx/variables/arrays/)
--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// Дан массив:
+
 // function App() {
-//   return (
-//     <>
-//       <ul>
-//         <li>text1</li>
-//         <li>text2</li>
-//         <li>text3</li>
-//       </ul>
-//       <ul>
-//         <li>text1</li>
-//         <li>text2</li>
-//         <li>text3</li>
-//       </ul>
-//       ;
-//     </>
-//   );
+// 	const arr = [1, 2, 3, 4, 5];
 // }
+// Сделайте так, чтобы результатом рендеринга был тег ul, в тегах li которого будут стоять элементы массива.
 
 function App() {
-  return (
-    <div>
-      <ul>
-        <li>text1</li>
-        <li>text2</li>
-        <li>text3</li>
-      </ul>
-      <ul>
-        <li>text1</li>
-        <li>text2</li>
-        <li>text3</li>
-      </ul>
-      ;
-    </div>
-  );
+  const arr = [1, 2, 3, 4, 5, 6];
+  const ul = arr.map((el, ind) => {
+    return <li key={ind}>{el}</li>;
+  });
+  return <ul>{ul}</ul>;
 }
 
 export default App;
