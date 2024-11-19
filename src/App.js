bd91ae6863c7c5fa986d1b0736eae29d19bdc900
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-19 14:16:10 +0100

    Boolean in state

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 
-// В компоненте App дан следующий массив:
+function id() {
+  return nanoid(5);
+}
 
-const users = [
-  { id: 1, name: "user1", surn: "surn1", age: 30 },
-  { id: 2, name: "user2", surn: "surn2", age: 31 },
-  { id: 3, name: "user3", surn: "surn3", age: 32 },
-  { id: 3, name: "user3", surn: "surn3", age: 32 },
-];
-// Выведите элементы этого массива в виде таблицы table так, чтобы каждое поле объекта попало в свой тег td. Сделайте заголовки колонок вашей таблицы.
+// 1) Сделайте кнопку, нажатие на которую будет банить пользователя и кнопку, нажатие на которую будет разбанивать пользователя.
+
+// 2) Модифицируйте предыдущую задачу так, чтобы из двух кнопок всегда была видна только соответствующая. То есть, если пользователь забанен, то видна кнопка для разбанивания, а если не забанен - для забанивания.
 
 function App() {
-  const table = users.map(({ id, name, surn, age }) => {
-    return (
-      <tr key={id}>
-        <td>{name}</td>
-        <td>{surn}</td>
-        <td>{age}</td>
-      </tr>
-    );
-  });
+  const [isBan, setIsBan] = useState(false);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>name111</td>
-
-          <td>surn22</td>
-
-          <td>age333</td>
-        </tr>
-      </thead>
-      <tbody>{table}</tbody>
-    </table>
+    <>
+      <div>
+        <span>{isBan ? "nope" : "you ban"}</span>
+        <button onClick={() => setIsBan(!isBan)}>
+          {isBan ? "ban user" : "unbanned user"}
+        </button>
+      </div>
+    </>
   );
 }
 export default App;
