6201f76a78959fb8c6207935bb18752c0906c4be
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-19 14:42:30 +0100

    working with multiple inputs

--- src/App.js
+++ src/App.js
@@ -7,23 +7,37 @@ function id() {
   return nanoid(5);
 }
 
-// Сделайте два инпута. Пусть текст первого инпута выводится в первый абзац, а текст второго инпута - во второй абзац.
+// Даны 4 инпутов. Сделайте так, чтобы при вводе чисел в наши инпуты в абзац выводилось среднее арифметическое введенных чисел.
 
 function App() {
   const [value1, setValue1] = useState("");
   const [value2, setValue2] = useState("");
+  const [value3, setValue3] = useState("");
+  const [value4, setValue4] = useState("");
+
+  function handlerChange(ev, setState) {
+    setState(+ev.target.value);
+  }
+
   return (
     <>
       <input
         value={value1}
-        onChange={(ev) => setValue1(ev.target.value)}
+        onChange={(ev) => handlerChange(ev, setValue1)}
       ></input>
       <input
         value={value2}
-        onChange={(ev) => setValue2(ev.target.value)}
+        onChange={(ev) => handlerChange(ev, setValue2)}
       ></input>
-      <p>{value1}</p>
-      <p>{value2}</p>
+      <input
+        value={value3}
+        onChange={(ev) => handlerChange(ev, setValue3)}
+      ></input>
+      <input
+        value={value4}
+        onChange={(ev) => handlerChange(ev, setValue4)}
+      ></input>
+      <p>{(value1 + value2 + value3 + value4) / 2}</p>
     </>
   );
 }
