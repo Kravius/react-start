810415b2a8af782218858aa9d60f70a05120912f
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-19 13:31:18 +0100

    Array of Objects as an HTML Table

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-just put kod from history in searching on some browser ⊗jsrtPmFmAOO
-or [click](https://code.mu/ru/javascript/framework/react/book/prime/forming/array-of-objects/)
+just put kod from history in searching on some browser ⊗jsrtPmFmTA
+or [click](https://code.mu/ru/javascript/framework/react/book/prime/forming/table/)
--- src/App.js
+++ src/App.js
@@ -4,23 +4,37 @@ import "./App.css";
 // В компоненте App дан следующий массив:
 
 const users = [
-  { name: "user1", surn: "surn1", age: 30 },
-  { name: "user2", surn: "surn2", age: 31 },
-  { name: "user3", surn: "surn3", age: 32 },
+  { id: 1, name: "user1", surn: "surn1", age: 30 },
+  { id: 2, name: "user2", surn: "surn2", age: 31 },
+  { id: 3, name: "user3", surn: "surn3", age: 32 },
+  { id: 3, name: "user3", surn: "surn3", age: 32 },
 ];
-// Выведите элементы этого массива в виде списка ul.
+// Выведите элементы этого массива в виде таблицы table так, чтобы каждое поле объекта попало в свой тег td. Сделайте заголовки колонок вашей таблицы.
 
 function App() {
-  const text = users.map((user, index) => {
-    const data = Object.entries(user);
-    const span = data.map(([key, value], index) => (
-      <span key={index}>
-        {key}:{value}
-      </span>
-    ));
-    return <li key={index}>{span}</li>;
+  const table = users.map(({ id, name, surn, age }) => {
+    return (
+      <tr key={id}>
+        <td>{name}</td>
+        <td>{surn}</td>
+        <td>{age}</td>
+      </tr>
+    );
   });
 
-  return <ul>{text}</ul>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>name111</td>
+
+          <td>surn22</td>
+
+          <td>age333</td>
+        </tr>
+      </thead>
+      <tbody>{table}</tbody>
+    </table>
+  );
 }
 export default App;
