a8902fc815e93d92ab84255207655816abfea96b
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-27 15:34:12 +0100

    feat:checkbox and conditional rerendering

--- src/App.js
+++ src/App.js
@@ -7,116 +7,52 @@ function id() {
   return nanoid(5);
 }
 
-// Дан чекбокс, кнопка и абзац. По клику на кнопку, если чекбокс отмечен, выведите в абзац текст приветствия с пользователем, а если не отмечен - текст прощания.
+// Дан чекбокс. С помощью чекбокса спросите у пользователя, если ли ему уже 18 лет. Если чекбокс отмечен, покажите пользователю следующий блок кода:
+{
+  /* <div>
+	<h2>Ура, вам уже есть 18</h2>
+	<p>
+		здесь расположен контент только для взрослых
+	</p>
+</div>
+А если чекбокс не отмечен - то следующий:
 
-// function App() {
-//   const [checkBox, setCheckBox] = useState(false);
-//   const [textP, setTextP] = useState("");
-//   const [] = useState("");
-//   return (
-//     <>
-//       <input
-//         type="checkbox"
-//         checked={checkBox}
-//         onChange={() => setCheckBox(!checkBox)}
-//       ></input>
-//       <button onClick={() => (checkBox ? setTextP("HI") : setTextP("bey"))}>
-//         click
-//       </button>
-//       <p>состояние {textP}</p>
-//     </>
-//   );
-// }
-// export default App;
+ <div>
+	<p>
+		увы, вам еще нет 18 лет:(
+	</p>
+</div> */
+}
 
-// С помощью трех чекбоксов попросите пользователя выбрать языки, которые он знает: html, css и js. Результат выбора по каждому языку выводите в отдельные абзацы.
+const text1 = (
+  <div>
+    <h2>Ура, вам уже есть 18</h2>
+    <p>здесь расположен контент только для взрослых</p>
+  </div>
+);
+const text2 = (
+  <div>
+    <p>увы, вам еще нет 18 лет:</p>
+  </div>
+);
 
-// first try
-
-// function App() {
-//   // const language = ['js', 'css', 'html'];
-//   const [checkJS, setCheckJS] = useState(false);
-//   const [checkCSS, setCheckCSS] = useState(false);
-//   const [checkHTML, setCheckHTML] = useState(false);
-
-//   const [value, setValue] = useState("");
-
-//   function text(setUseSate, check, ev) {
-//     setUseSate(!check);
-//     console.log(ev.target.value);
-//   }
-
-//   return (
-//     <>
-//       <label>
-//         <input
-//           type="checkbox"
-//           checked={checkJS}
-//           onChange={(ev) => text(setCheckJS, checkJS, ev)}
-//           value={"js"}
-//         ></input>
-//         js
-//       </label>
-//       <label>
-//         <input
-//           type="checkbox"
-//           checked={checkCSS}
-//           onChange={(ev) => text(setCheckCSS, checkCSS, ev)}
-//           value={"css"}
-//         ></input>
-//         css
-//       </label>
-
-//       <label>
-//         <input
-//           type="checkbox"
-//           checked={checkHTML}
-//           onChange={(ev) => text(setCheckHTML, checkHTML, ev)}
-//           value="HTML"
-//         ></input>
-//         html
-//       </label>
-//       <p>{checkJS ? "js" : false}</p>
-//       <p>{checkCSS ? "css" : false}</p>
-//       <p>{checkHTML ? "html" : false}</p>
-//     </>
-//   );
-// }
-
-//second try
 function App() {
-  const language = ["js", "css", "html"];
-  const [checkedLanguage, setCheckedLanguage] = useState(
-    language.reduce((acc, lang) => ({ ...acc, [lang]: false }), {})
-  );
+  const [checked, setChecked] = useState(false);
 
-  function changeBox(boxLang) {
-    setCheckedLanguage((prev) => ({ ...prev, [boxLang]: !prev[boxLang] }));
-  }
-
-  function createInputs() {
-    return language.map((lang) => (
-      <label key={id()}>
-        <input
-          key={id()}
-          type="checkBox"
-          checked={checkedLanguage[lang]}
-          onChange={() => changeBox(lang)}
-        ></input>
-        {lang}
-      </label>
-    ));
-  }
-
-  function createText() {
-    return language.map(
-      (text) => checkedLanguage[text] && <p key={id()}>{text}</p>
-    );
+  function howOld() {
+    return (checked && text1) || text2;
   }
   return (
     <>
-      {createInputs()}
-      <div>{createText()}</div>
+      <label>
+        <input
+          checked={checked}
+          type="checkBox"
+          onChange={() => setChecked(!checked)}
+        ></input>
+        do you have 18?
+      </label>
+      {howOld()}
     </>
   );
 }
