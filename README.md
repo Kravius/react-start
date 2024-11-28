36948794415bc183300388cf69f586b913ce2146
Glib Shemenkov <shemenkovglib@gmail.com>
2024-11-28 14:34:54 +0100

    feat:radio buttons

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-just put kod from history in searching on some browser ⊗jsrtPmFmsSV
-or [click](https://code.mu/ru/javascript/framework/react/book/prime/forms/select/value/)
+just put kod from history in searching on some browser ⊗jsrtPmFmsRd
+or [click](https://code.mu/ru/javascript/framework/react/book/prime/forms/radio/)
--- src/App.js
+++ src/App.js
@@ -9,36 +9,63 @@ function id() {
 
 // С помощью выпадающего списка предложите пользователю выбрать к какой возрастной группе он относится: от 0 до 12 лет, от 13 до 17, от 18 до 25, либо старше 25 лет.
 
-function App() {
-  const [groupOption, setGroupOption] = useState("");
-  const ageGroup = {
-    first: "0 12",
-    second: "13 17",
-    third: "18 25",
-    fours: "25",
-  };
+// function App() {
+//   const [radioValue, setRadioValue] = useState("1");
+//   const radioArray = ["1", "2", "3"];
 
-  function selectOption(value) {
-    return <p>{ageGroup[value] || ageGroup.first}</p>;
+//   function changeHandlerRadio(ev) {
+//     setRadioValue(ev.target.value);
+//   }
+
+//   const createRadio = radioArray.map((el) => (
+//     <input
+//       type="radio"
+//       value={el}
+//       name="radioTest"
+//       checked={radioValue === el && radioValue}
+//       key={id()}
+//       onChange={(ev) => changeHandlerRadio(ev)}
+//     ></input>
+//   ));
+//   return <>{createRadio}</>;
+// }
+// export default App;
+
+// С помощью радиокнопочек спросите у пользователя его любимый язык программирования. Выведите его выбор в абзац. Если выбран язык JavaScript, похвалите пользователя.
+
+function App() {
+  const [radioValue, setRadioValue] = useState("");
+  const [radioValueText, setRadioValueText] = useState("");
+  // const radioArray = ["1", "2", "3"];
+  const radioLanguages = { 1: "js", 2: "html", 3: "css" };
+
+  function changeHandlerRadio(ev) {
+    setRadioValue(ev.target.value);
   }
-  const createSelect = (
-    <select
-    //always show a text from option when we  use select but we also can chose value
-      value={groupOption}
-      onChange={(ev) => setGroupOption(ev.target.value)}
-    >
-      {Object.entries(ageGroup).map(([group, ageText]) => (
-        <option key={id()} value={group}>
-          {ageText}
-        </option>
-      ))}
-    </select>
-  );
+
+  const createRadio = Object.entries(radioLanguages).map(([el, text]) => (
+    <label value={text} key={id()}>
+      {text}
+      <input
+        type="radio"
+        value={el}
+        name="radioTest"
+        checked={radioValue === el && radioValue}
+        key={id()}
+        onChange={(ev) => changeHandlerRadio(ev)}
+      ></input>
+    </label>
+  ));
   return (
     <>
-      {createSelect}
-      <p>{groupOption}</p>
-      {selectOption(groupOption)}
+      <div>
+        <p>what is your favorites language?</p>
+        {createRadio}
+        <p>
+          {radioLanguages[radioValue]}:
+          {radioLanguages[radioValue] === "js" && "good job"}
+        </p>
+      </div>
     </>
   );
 }
