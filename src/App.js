a87f6ab18b6b5d0a8b76d7fbf26d5fa10f839d12
Glib Shemenkov <shemenkovglib@gmail.com>
2024-12-02 21:55:45 +0100

    reactive array operations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { nanoid } from "nanoid";
 import { useState } from "react";
@@ -7,44 +6,127 @@ function id() {
   return nanoid(5);
 }
 
-// Сделайте кнопку, по нажатию на которую будет добавляться новый элемент в конец массива, тем самым добавляя новый li в конец тега ul.
+//1 Дан массив с числами. Выведите его в виде списка ul. По клику на любую li возведите ее число в квадрат.
 
-//Сделайте инпут и кнопку. По нажатию на кнопку пусть текст инпута станет новым тегом li в конец тега ul.
+//2 В конце каждой li сделайте кнопку, по нажатию на которую эта li будет удаляться из списка.
+
+//3 Дан инпут. По клику на любую li сделайте так, чтобы текст этой li попал в инпут.
+
+//4 Модифицируйте предыдущую задачу так, чтобы по потери фокуса инпута измененный текст попадал в соответствующую li.
+
+//5 Дана кнопка. По клику на эту кнопку переверните порядок li на обратный.
 
 function App() {
   const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-  const [valueInp, setValueInp] = useState("");
 
-  const li = notes.map((el, ind) => <li key={ind}>{el}</li>);
+  //мое решение задачи 3-4
+  // const [inputValue, setInputValue] = useState({ index: "" });
 
-  const input = (
-    <input
-      value={valueInp}
-      placeholder="write a new one li"
-      onChange={(ev) => setValueInp(ev.target.value)}
-    ></input>
-  );
+  const [indexInput, setIndexInput] = useState(null);
+  const [valueInput, setValueInput] = useState("");
 
   //first task
-  // function addLi() {
-  //   setNotes((prev) => [...prev, +prev[prev.length - 1] + 1]);
+  // function squareLi(ev, index) {
+  //   const newNote = [...notes];
+  //   newNote[index] = newNote[index] ** 2;
+  //   setNotes(newNote);
   // }
 
-  function addLi() {
-    if (valueInp.length) {
-      const newLi = [...notes];
-      newLi.push(valueInp);
-      setNotes(newLi);
-      setValueInp("");
-    }
+  function changeLi(index) {
+    setValueInput(notes[index]);
+    setIndexInput(index);
   }
 
-  const btn = <button onClick={addLi}>click add</button>;
+  const lis = notes.map((el, index) => (
+    //first task
+    // <li key={index} onClick={(ev) => squareLi(ev, index)}>
+
+    <li key={index} onClick={(ev) => changeLi(index)}>
+      {el}
+      {createBtn(index)}
+    </li>
+  ));
+
+  function createBtn(index) {
+    return <button onClick={(ev) => deleteLi(ev, index)}>delete</button>;
+  }
+
+  function deleteLi(ev, index) {
+    ev.stopPropagation();
+    setNotes((prev) => [...prev.slice(0, index), ...prev.slice(index + 1)]);
+  }
+
+  //мое решение задачи 3-4
+
+  // function changeLi(index) {
+  //   setInputValue({ [index]: notes[index] });
+  // }
+
+  // function handleChangeInputs(ev) {
+  //   setInputValue({ [Object.keys(inputValue)]: ev.target.value });
+  // }
+
+  // //мое решение задачи 3-4
+  // const input = (
+  //   <label>
+  //     change li:
+  //     <input
+  //       value={inputValue[Object.keys(inputValue)[0]]}
+  //       onChange={(ev) => handleChangeInputs(ev)}
+  //       onBlur={(ev) => handleChangeLi(ev)}
+  //     ></input>
+  //   </label>
+  // );
+
+  // function handleChangeLi(ev) {
+  //   const keyLi = Object.keys(inputValue)[0];
+  //   if (keyLi !== "index") {
+  //     const newLi = [...notes];
+  //     newLi[keyLi] = ev.target.value;
+  //     setNotes(newLi);
+  //   }
+  //   setInputValue({ index: "" });
+  // }
+
+  const input = (
+    <label>
+      change li:
+      <input
+        value={valueInput}
+        onChange={(ev) => handleChangeInputs(ev)}
+        onBlur={(ev) => handleChangeLi(ev)}
+      ></input>
+    </label>
+  );
+
+  function handleChangeInputs(ev) {
+    setValueInput(ev.target.value);
+  }
+
+  function handleChangeLi(ev) {
+    if (indexInput !== null) {
+      const newLi = [...notes];
+      newLi[indexInput] = ev.target.value;
+      setNotes(newLi);
+    }
+    setValueInput("");
+  }
+
+  const createReverseBtn = (
+    <button onClick={handleReverse}>reverse click</button>
+  );
+
+  function handleReverse() {
+    setNotes((prev) => [...prev].reverse());
+  }
+
   return (
     <>
-      <ul>{li}</ul>
-      {input}
-      {btn}
+      <ul>{lis}</ul>
+      <div>
+        {input}
+        {createReverseBtn}
+      </div>
     </>
   );
 }
