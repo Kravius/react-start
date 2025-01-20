bf2d5b136ec3141eae7a2835eb71d2536fcd49de
Glib Shemenkov <shemenkovglib@gmail.com>
2025-01-20 15:02:46 +0100

    feat: layout routes

--- app/root.tsx
+++ app/root.tsx
@@ -6,67 +6,11 @@ import {
   Outlet,
   Link,
 } from "react-router";
+import appStylesHref from "./app.css?url";
 import type { Route } from "./+types/root";
 
-import appStylesHref from "./app.css?url";
-import { getContacts } from "./data";
-
-export async function clientLoader() {
-  const contacts = await getContacts();
-  return { contacts };
-}
-
-export default function App({ loaderData }: Route.ComponentProps) {
-  const { contacts } = loaderData;
-  return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
-              aria-label="Search contacts"
-              id="q"
-              name="q"
-              placeholder="Search"
-              type="search"
-            />
-            <div aria-hidden hidden={true} id="search-spinner" />
-          </Form>
-          <Form method="post">
-            <button type="submit">New</button>
-          </Form>
-        </div>
-        <nav>
-          {contacts.length ? (
-            <ul>
-              {contacts.map((contact) => (
-                <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
-                    {contact.first || contact.last ? (
-                      <>
-                        {contact.first} {contact.last}
-                      </>
-                    ) : (
-                      <i> no Name</i>
-                    )}
-                    {contact.favorite ? <span>★</span> : null}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>
-              <i>No contacts</i>
-            </p>
-          )}
-        </nav>
-      </div>
-      <div id="detail">
-        <Outlet />
-      </div>
-    </>
-  );
+export default function App() {
+  return <Outlet />;
 }
 
 //автоматически подключает к нашему проекту.
